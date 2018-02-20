1d97d3c669e2ffe93e762d5734288cef635b7151
RickWagenmakers <rpwwagen@avans.nl>
2018-02-20 17:16:33 +0100

    table model tekst in tabel

--- src/Gui/TablePanel.java
+++ src/Gui/TablePanel.java
@@ -29,8 +29,10 @@ public class TablePanel extends JPanel {
         JTable tablePanel = new JTable(new Table());
         panel.add(tablePanel);
         tablePanel.setRowHeight(50);
-        tablePanel.setPreferredSize(new Dimension(800, 800));
+        tablePanel.setPreferredSize(new Dimension(750, 750));
 
+        DefaultTableModel model = new DefaultTableModel();
+        
 
 
         frame.setSize(800, 800);
@@ -47,24 +49,44 @@ public class TablePanel extends JPanel {
     private class Table extends AbstractTableModel {
         public int getRowCount()
         {
-            return 1;
+            return 2;
         }
 
         public int getColumnCount()
         {
-            return 5;
+            return 6;
         }
 
         public Object getValueAt(int rowIndex, int columnIndex) {
 
-            program.getActs(0);
-            System.out.println(program.getActs(rowIndex).getStage().getName());
+
             switch (columnIndex)
             {
-                case 0: program.getActs(0).getStage();
+                case 0: return program.getActs(rowIndex).getStage().getName();
+                case 1: return program.getActs(rowIndex).getArtist().getName();
+                case 2: return program.getActs(rowIndex).getArtist().getGenre();
+                case 3: return program.getActs(rowIndex).getStartTime();
+                case 4: return program.getActs(rowIndex).getEndTime();
+                case 5: return program.getActs(rowIndex).getPopularity();
             }
 
             return "";
+        }
+
+        @Override
+        public String getColumnName(int column) {
+
+            switch (column)
+            {
+                case 0: return "Podium";
+                case 1: return "Artiest";
+                case 2: return "Genre";
+                case 3: return "Begintijd";
+                case 4: return "Eindtijd";
+                case 5: return "Populariteit Act";
+            }
+
+            return ""  ;
         }
     }
 }
--- src/agenda/TestDataAgenda.java
+++ src/agenda/TestDataAgenda.java
@@ -20,12 +20,14 @@ public class TestDataAgenda
 
         Program program = new Program();
         Artist artist1 = new Artist("ALIB", 30, "Rap");
-        Artist artist2 = new Artist("Guus Meeuws", 70, "Nederlands");
+        Artist artist2 = new Artist("Guus Meeuwis", 70, "Nederlands");
         Stage stage = new Stage("P1", 1000, 50, 50);
+        Stage stage1 = new Stage("P2", 1000,50,50);
         program.addAct(artist2, stage, 2000, 2100,50);
+        program.addAct(artist1,stage1,2200,2300,100);
 
-       // program.save(program);
+      program.save(program);
 
-        System.out.println(program.load());
+       System.out.println(program.load());
     }
 }
