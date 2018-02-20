4edd1bf74a390c42cbaf3a916e45000f2251918a
Boudewijn <boudewijn348@gmail.com>
2018-02-20 14:07:26 +0100

    [add] Gui act objects maker

---
+++ src/Gui/ActObject.java
@@ -0,0 +1,39 @@
+package Gui;
+
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class ActObject {
+    private RoundRectangle2D.Double rectangle;
+    private Color color;
+
+    /*
+    Creates a rectangle that with the right color.
+     */
+    public ActObject(int x, int y, int width, int height ){
+    this.rectangle = new RoundRectangle2D.Double(x, y ,  width, height , 10 , 10);
+    this.color = new Color(153, 204 ,255);
+    }
+
+    public int getX() {
+        return (int)this.rectangle.getX();
+    }
+
+    public int getY() {
+        return (int)this.rectangle.getY();
+    }
+
+    private int getWidth() {
+        return (int)this.rectangle.getWidth();
+    }
+
+    private int getHeight() {
+        return (int)this.rectangle.getHeight();
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(this.color);
+        g.fillRoundRect(this.getX(), this.getHeight() ,  this.getWidth(), this.getHeight() , 10 , 10);
+        g.setColor(new Color(51, 51, 51));
+    }
+}
