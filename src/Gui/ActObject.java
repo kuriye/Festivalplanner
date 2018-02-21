6aa7523c9750a50c685e637ea1ae385f666dad21
Boudewijn <boudewijn348@gmail.com>
2018-02-21 14:47:51 +0100

    updating the ArcObject
    now able to add actor begintime and endtime

--- src/Gui/ActObject.java
+++ src/Gui/ActObject.java
@@ -6,13 +6,19 @@ import java.awt.geom.RoundRectangle2D;
 public class ActObject {
     private RoundRectangle2D.Double rectangle;
     private Color color;
+    private String actor;
+    private String beginTime;
+    private String endTIme;
 
     /*
     Creates a rectangle that with the right color.
      */
-    public ActObject(int x, int y, int width, int height ){
+    public ActObject(int x, int y, int width, int height, String actor, String beginTime, String endTime){
     this.rectangle = new RoundRectangle2D.Double(x, y ,  width, height , 10 , 10);
     this.color = new Color(153, 204 ,255);
+    this.actor = actor;
+    this. beginTime = beginTime;
+    this.endTIme = endTime;
     }
 
     public int getX() {
@@ -35,5 +41,8 @@ public class ActObject {
         g.setColor(this.color);
         g.fillRoundRect(this.getX(), this.getY() ,  this.getWidth(), this.getHeight() , 10 , 10);
         g.setColor(new Color(51, 51, 51));
+        g.drawString("" + actor, (this.getX() + this.getWidth())/2, (this.getY() + this.getHeight())-5);
+        g.drawString("" + beginTime, (this.getX() + this.getWidth())/2, (this.getY() + this.getHeight())-10);
+        g.drawString("" + endTIme, (this.getX() + this.getWidth())/2, (this.getY() + this.getHeight())-15);
     }
 }
