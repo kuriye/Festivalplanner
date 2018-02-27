83d5452aa23b4ec4c263deb5660b261d99c157cb
RickWagenmakers <rpwwagen@avans.nl>
2018-02-27 16:01:39 +0100

    Auto stash before merge of "newDevelop" and "origin/newDevelop"
    create class TiledTile with constuctor and method getBufferdImage

--- src/maplogic/TiledMap.java
+++ src/maplogic/TiledMap.java
@@ -1,4 +1,19 @@
 package maplogic;
 
-public class TiledMap {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Map;
+
+public class TiledMap extends JPanel {
+
+    public TiledMap()
+    {
+
+
+    }
+
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 }
--- src/maplogic/TiledTile.java
+++ src/maplogic/TiledTile.java
@@ -1,4 +1,30 @@
 package maplogic;
 
+import java.awt.image.BufferedImage;
+
 public class TiledTile {
+
+    TiledLayer tiledLayer;
+    TiledTileMap tiledTileMap;
+
+
+
+    public TiledTile(TiledLayer tiledLayer) {
+
+        this.tiledLayer = tiledLayer;
+    }
+
+    public BufferedImage getBufferdImage(int index)
+    {
+        BufferedImage[] images = tiledTileMap.getArray();
+        if(index == 0)
+        {
+            return null;
+        }
+        else
+        {
+            return images[index -1];
+        }
+
+    }
 }
