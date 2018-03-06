8238cfea0318fbe567e4aa625d0f602cc12a71e0
Olaf van Wijngaarden <odwijnga@avans.nl>
2018-03-06 15:18:28 +0100

    [added] Clipping for less lag

--- src/maplogic/Test.java
+++ src/maplogic/Test.java
@@ -1,18 +1,16 @@
 package maplogic;
 
-import jdk.nashorn.internal.ir.Block;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class Test extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener {
     private Camera camera;
     private static TiledMap test;
     private boolean scrolled = false;
-    private int amountOfScrolled = 0;
+    private int amountScrolled = 0;
     private int yPositionScroll = 0;
 
     public static void main(String[] args) {
@@ -25,18 +23,28 @@ public class Test extends JPanel implements MouseListener, MouseMotionListener, 
         frame.setVisible(true);
     }
 
+    Test()
+    {
+        addMouseListener(this);
+        addMouseWheelListener(this);
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        test.draw(g2d);
+
+        Shape screen = new Rectangle2D.Double(0, 0, getWidth(), getHeight());
+        g2d.setClip(screen);
 
         if (camera == null) camera = new Camera(new Point2D.Double(getWidth(), getHeight()), 0.5f);
-
+        test.draw(g2d);
         g2d.setTransform(camera.getTransform());
 
 //        for(Block block : blocks) {
 //            block.draw(g2d);
 //        }
+
+
     }
 
     @Override
@@ -82,14 +90,14 @@ public class Test extends JPanel implements MouseListener, MouseMotionListener, 
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
         scrolled = true;
-        amountOfScrolled = e.getWheelRotation();
+        amountScrolled = e.getWheelRotation();
         repaint();
     }
 
     protected void ifScrolled(Graphics2D g2d){
         if (scrolled){
             scrolled = false;
-            if (amountOfScrolled < 0){
+            if (amountScrolled < 0){
                 if (yPositionScroll >= 0){
                     g2d.translate(0,0);
                 }
@@ -98,7 +106,7 @@ public class Test extends JPanel implements MouseListener, MouseMotionListener, 
                     g2d.translate(0,yPositionScroll);
                 }
             }
-            else if(amountOfScrolled > 0){
+            else if(amountScrolled > 0){
                 if(yPositionScroll < -1350){
                     g2d.translate(0,-1400);
                 }
