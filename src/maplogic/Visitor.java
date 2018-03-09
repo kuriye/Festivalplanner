2e1e7cbdf460d4d856e40e08053e88b95d97b0da
Whisper <gewoonjvr@gmail.com>
2018-03-09 13:22:07 +0100

    [added] java doc to visitor.
    added java doc to the visitor class, not completed yet

--- src/maplogic/Visitor.java
+++ src/maplogic/Visitor.java
@@ -8,7 +8,9 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
+/**
+ *
+ */
 public class Visitor {
     private Point2D position;
     private double angle;
@@ -16,6 +18,9 @@ public class Visitor {
     private double speed;
     private Point2D targetPosition = new Point2D.Double(100,100);
 
+    /**
+     * Creates a visitor object wich will walk around the festival.
+     */
     public Visitor()
     {
         position = new Point2D.Double(1000,500);
@@ -28,13 +33,19 @@ public class Visitor {
         }
     }
 
-    public void draw(Graphics2D g2d) {
-        AffineTransform tx = new AffineTransform();
+    /**
+     * draws the visitor on the panel.
+     * @param g2d is contained from a paintcomponent from a panel.
+     */
+    public void draw(Graphics2D g2d, AffineTransform tx) {
         tx.translate(position.getX() - image.getWidth()/2, position.getY() - image.getHeight()/2);
-        tx.rotate(angle, image.getWidth()/2, image.getHeight()/2);
         g2d.drawImage(image, tx, null);
     }
 
+    /**
+     * Updates the position of the visitors.
+     * @param visitors checks if visitors don't collide with other visitors.
+     */
     public void update(ArrayList<Visitor> visitors) {
         int directionX = 0;
         int directionY = 0;
@@ -43,6 +54,7 @@ public class Visitor {
                 targetPosition.getY() - position.getY()
         );
 
+        //calculates wich way the visitor needs to go. First the x-coordinate then the y.
         if (position.getX() != targetPosition.getX()){
             if (diff.getX() <= 0){
                 directionX = -1;
@@ -73,6 +85,11 @@ public class Visitor {
         }
     }
 
+    /**
+     * checks if some visitor collides
+     * @param visitors contains all the visitors including itself.
+     * @return boolean if collision is true or not.
+     */
     public boolean hasCollision(ArrayList<Visitor> visitors) {
         boolean hasCollision = false;
         for(Visitor visitor : visitors)
@@ -86,6 +103,10 @@ public class Visitor {
         return hasCollision;
     }
 
+    /**
+     * sets targetPosition where the visitor will be going.
+     * @param targetPosition
+     */
     public void setTarget(Point2D targetPosition) {
         this.targetPosition = targetPosition;
     }
