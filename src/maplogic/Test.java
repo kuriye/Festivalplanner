0b55ee2d27c1b6ee4b761051f27f609d99efc33d
Tom Martens <ketsh11@hotmail.com>
2018-03-09 12:11:03 +0100

    [Edit] Fixed camera class, removed mouselisteners from test

--- src/maplogic/Test.java
+++ src/maplogic/Test.java
@@ -8,18 +8,15 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
-public class Test extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener, ActionListener {
+public class Test extends JPanel implements ActionListener {
     private Camera camera;
     private static TiledMap test;
     private boolean scrolled = false;
     private int amountScrolled = 0;
     private int yPositionScroll = 0;
     private ArrayList<Visitor> visitors;
-    private Point2D startDragPos;
-    private Point2D endDragPos;
 
     public static void main(String[] args) {
-        test = new TiledMap("/dikkefix.json");
         JFrame frame = new JFrame("Simulatie");
         frame.setPreferredSize(new Dimension(800, 800));
         frame.setMinimumSize(new Dimension(800, 600));
@@ -28,27 +25,21 @@ public class Test extends JPanel implements MouseListener, MouseMotionListener, 
         frame.setVisible(true);
     }
 
-    public Test()
-    {
+    public Test() {
+        test = new TiledMap("/dikkefix.json");
         visitors = new ArrayList<>();
 
-        while(visitors.size() < 20)
-        {
+        while (visitors.size() < 20) {
             Visitor visitor = new Visitor();
-            if(!visitor.hasCollision(visitors))
+            if (!visitor.hasCollision(visitors))
                 visitors.add(visitor);
         }
 
-        Timer t = new Timer(1000/60,this);
+        Timer t = new Timer(1000 / 60, this);
         t.start();
 
-        addMouseListener(this);
-        addMouseMotionListener(this);
-
-
-        addMouseWheelListener(this);
-
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
@@ -59,94 +50,23 @@ public class Test extends JPanel implements MouseListener, MouseMotionListener, 
 
         //Camera movement registeren
         if (camera == null)
-            camera = new Camera(new Point2D.Double(0, 0), 0.5f);
-        test.draw(g2d, camera.getTransform());
+            camera = new Camera(this);
+        test.draw(g2d, camera.getTransform(getHeight(),getWidth()));
 
-        for(Visitor visitor: visitors)
-        {
-            visitor.draw(g2d , camera.getTransform());
+        for (Visitor visitor : visitors) {
+            visitor.draw(g2d, camera.getTransform(getHeight(),getWidth()));
         }
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        for(Visitor visitor: visitors)
-        {
+        for (Visitor visitor : visitors) {
             visitor.update(visitors);
         }
 
         repaint();
 
-    }
-
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        if(SwingUtilities.isRightMouseButton(e))
-        startDragPos = e.getPoint();
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        if(SwingUtilities.isRightMouseButton(e))
-            endDragPos = e.getPoint();
-    }
-
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        if(SwingUtilities.isRightMouseButton(e)) {
-            Point2D position = e.getPoint();
-            camera.setTarget(new Point2D.Double(e.getX() - startDragPos.getX(), e.getY() - startDragPos.getY()));
-            repaint();
-        }
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        for(Visitor visitor: visitors)
-        {
-            visitor.setTarget(e.getPoint());
-        }
-    }
-
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
-        scrolled = true;
-        amountScrolled = e.getWheelRotation();
-        repaint();
-    }
-
-    protected void ifScrolled(Graphics2D g2d){
-        if (scrolled){
-            scrolled = false;
-            if (amountScrolled < 0){
-                if (yPositionScroll >= 0){
-                    g2d.translate(0,0);
-                }
-                else{
-                    yPositionScroll += 25;
-                    g2d.translate(0,yPositionScroll);
-                }
-            }
-            else if(amountScrolled > 0){
-                if(yPositionScroll < -1350){
-                    g2d.translate(0,-1400);
-                }
-                else{
-                    yPositionScroll -= 25;
-                    g2d.translate(0,yPositionScroll);
-                }
-            }
-        }
     }
 }
 
