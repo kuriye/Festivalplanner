04a5dc98a2bac06e0c672ec4c73ea3347f573fcc
Whisper <gewoonjvr@gmail.com>
2018-03-13 10:36:19 +0100

    [added] methods and atributes for class Target

--- src/pathfinding/Target.java
+++ src/pathfinding/Target.java
@@ -1,4 +1,16 @@
 package pathfinding;
 
+import maplogic.TiledLayer;
+
 public class Target {
+    private TiledLayer targetLayer;
+    private TiledLayer collisionLayer;
+
+    public Target(){
+
+    }
+
+    public void calculateDistancesFromTarget(){
+
+    }
 }
